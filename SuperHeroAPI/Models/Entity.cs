a311bbe92f56e9c38cf4c968641f0d5f92b59d33
Thiago Pessoa <thiagokxl5@gmail.com>
2022-07-16 11:04:40 -0300

    Adicionar arquivos de projeto.

---
+++ SuperHeroAPI/Models/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperHeroAPI.Models
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+
+        public Entity()
+        {
+            var idRandom = new Random();
+            Id = idRandom.Next();
+        }
+    }
+}
---
+++ SuperHeroAPI/Models/SuperHero.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperHeroAPI.Models
+{
+    public class SuperHero : Entity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+    }
+}
